653687e6b98187ab05b3c7f808139f1c4bb639d4
Marcos Lima <48837929+MarcosACLima@users.noreply.github.com>
2022-03-09 15:17:39 -0300

    Domain model, database seed

---
+++ backend/src/main/java/com/devsuperior/dsmovie/entities/Score.java
@@ -0,0 +1,44 @@
+package com.devsuperior.dsmovie.entities;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_score")
+public class Score {
+
+	@EmbeddedId
+	private ScorePK id = new ScorePK(); // classe auxiliar com identificadores compostos
+	private Double value;
+
+	public Score() {
+	}
+
+	// Informar Movie e associar com Score -
+	public void setMovie(Movie movie) {
+		id.setMovie(movie);
+	}
+
+	// Informar User e associar com Score -
+	public void setUser(User user) {
+		id.setUser(user);
+	}
+
+	public ScorePK getId() {
+		return id;
+	}
+
+	public void setId(ScorePK id) {
+		this.id = id;
+	}
+
+	public Double getValue() {
+		return value;
+	}
+
+	public void setValue(Double value) {
+		this.value = value;
+	}
+
+}
